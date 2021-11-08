1101c79f8c0ad34be39a91e73d3384be5d8ab943
ashwin-thinkify <ak@thinkify.io>
2021-11-08 13:37:21 +0530

    Updated the content for them material ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,34 @@
-import '../styles/globals.css'
+import '../../styles/globals.css'
 import type { AppProps } from 'next/app';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../utils/theme';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  
+  return (
+      <div>
+        <Head>
+        <title>My page</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+      </div>
+  );
 }
 
 export default MyApp
+
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 import Particles from "react-tsparticles";
+import NavBar from '../components/Navbar';
 
 const Home: NextPage = () => {
 
@@ -21,11 +22,28 @@ const Home: NextPage = () => {
       <Head>
         <title>Thinkify</title>
         <meta name="description" content="thinkify is a recruting platform" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <NavBar />
       <main className={styles.main}>
+	  <div className="container">
+    <div className="row mt-4 mb-4">
+      <div className="col-sm-6">
+        <h1 className="text-xl md:text-3xl mb-5 text-white">
+        	A career coach in your pocket
+        </h1>
+        <h2 className="lead mb-5 leading-6 text-base md:text-xl text-white">
+          Find your dream job with the help of a certified career coach.
+        </h2>
+        <div className="mt-5">
+          <a href="/start" className="btn btn-primary btn-pill py-3 px-4 font-weight-semi letterspace-2">
+            Find Your Coach
+            <span className="mdi mdi-arrow-right mdi-21px m-l-1"></span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
         <Particles id="tsparticles" url="./particlesjs-config.json" init={particlesInit} loaded={particlesLoaded} />
       </main>
 
